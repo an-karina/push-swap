9a2938bab205c4f855ec8321ce5a0cb6fa1d5e3b
Карина Андрян <kshantel@Karinas-MacBook-Air.local>
2021-07-19 23:12:56 +0300

    norm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jhleena <jhleena@student.42.f>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 19:09:09 by jhleena           #+#    #+#             */
-/*   Updated: 2021/07/19 19:06:15 by jhleena          ###   ########.fr       */
+/*   Updated: 2021/07/19 23:12:37 by jhleena          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,19 +54,36 @@ void	algo_for_three(char **argv)
 	}
 }
 
-int	create_list(int argc, char *argv[])
+t_commands	*using_algorithm(char **argv,int (*markup_header)(t_list *stack, t_list *elem))
 {
 	t_list		*stack_a;
-	t_list		*tmp_stack_a;
 	t_list		*stack_b;
+	t_commands	*cmd_ind;
+	char		**argv_beg;
+	
+	stack_a = NULL;
+	stack_b = NULL;
+	argv_beg = argv;
+	while (*argv)
+	{
+		if (!create_stack_a(*argv, &stack_a))
+			return (free_list(&stack_a), NULL);
+		++argv;
+	}
+	cmd_ind = mark_up(&stack_a, &stack_b, markup_header);
+	argv =  argv_beg;
+	return (cmd_ind);
+}
+
+int	create_list(int argc, char *argv[])
+{
+	t_list		*tmp_stack_a;
 	t_commands	*cmd_ind;
 	t_commands	*cmd_gt;
 	char		*tmp_c;
 	char		**tmp_argv;
 	int			length;
 
-	stack_a = NULL;
-	stack_b = NULL;
 	tmp_stack_a = NULL;
 	if (argc == 1)
 		return (write(2, "Error: Wrong number of arguments\n", 34), FALSE);
@@ -76,20 +93,8 @@ int	create_list(int argc, char *argv[])
 		return (0);
 	}
 	tmp_argv = argv;
-	while (*argv)
-	{
-		if (!create_stack_a(*argv, &stack_a))
-			return (free_list(&stack_a), FALSE);
-		++argv;
-	}
-	cmd_ind = mark_up(&stack_a, &stack_b, &get_index);
-	while (*tmp_argv)
-	{
-		if (!create_stack_a(*tmp_argv, &tmp_stack_a))
-			return (free_list(&tmp_stack_a), FALSE);
-		++tmp_argv;
-	}
-	cmd_gt = mark_up(&tmp_stack_a, &stack_b, &greater_than);
+	cmd_ind = using_algorithm(argv, &get_index);
+	cmd_gt = using_algorithm(argv, &greater_than);
 	length = check_length(cmd_ind, cmd_gt);
 	if (length == 0)
 	{
